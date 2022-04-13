c279013c69a8890d5215f7a3186be2bac4aa556c
noear <noear@live.cn>
2022-04-13 16:14:23 +0800

    1.0.0

---
+++ src/test/java/com/swagger/demo/App.java
@@ -0,0 +1,14 @@
+package com.swagger.demo;
+
+
+import org.noear.solon.Solon;
+
+/**
+ * @author lbq
+ * @date 2020/06/16
+ */
+public class App {
+    public static void main(String[] args) {
+        Solon.start(App.class, args);
+    }
+}
